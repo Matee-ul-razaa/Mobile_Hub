1a4f5e180013ec2b35eae6b14f35994ea4f75f63
danish <killerraza640.dj@gmail.com>
2026-04-26 23:52:28 +0500

    Fix: Added missing DataProvider to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import 'chart.js/auto'
 
+import { DataProvider } from './DataContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>,
 )
