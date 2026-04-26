0968a6bd08d47d8871f775fa425296b681e87113
danish <killerraza640.dj@gmail.com>
2026-04-26 23:49:34 +0500

    Fix: Bypassing Hobby plan function limit by using _lib folder

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const apiRoutes = require('./routes/api');
+const apiRoutes = require('./_lib/routes/api');
 
 const app = express();
 
